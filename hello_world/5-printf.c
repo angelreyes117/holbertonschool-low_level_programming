65b780f7b73ff6883879ea1179feeae4e0f35b3c
angelreyes117 <angelreyes.117@gmail.com>
2024-10-25 06:09:23 +0000

    Printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * Description: This program prints a specific message to the
+ * standard output using the printf function.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
