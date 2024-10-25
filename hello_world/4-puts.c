4a3afaf2e829d740fd09bdb784357e6a0cef7e64
angelreyes117 <angelreyes.117@gmail.com>
2024-10-25 05:32:14 +0000

    Puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Returns Always 0 
+ *
+ * Description: This program prints a specific message to the
+ * standard output using the puts function, which outputs a
+ * string followed by a new line.
+ */
+
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return(0);
+}
