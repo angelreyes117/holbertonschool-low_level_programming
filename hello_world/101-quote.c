87b1e410f66d6fa2ab6762d99bd873e2adecf680
angelreyes117 <angelreyes.117@gmail.com>
2024-10-26 06:06:52 +0000

    Quote

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -6,9 +6,10 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 return (1);
 }
+
 
 
 
