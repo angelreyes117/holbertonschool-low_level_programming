e38e8f5f151b37bffb825b6e98337182ddecbbf2
angelreyes117 <angelreyes.117@gmail.com>
2024-10-29 04:41:02 +0000

    100 times table

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -6,39 +6,41 @@
  */
 void print_times_table(int n)
 {
-    int row, col, product;
+	int row, col, product;
 
-    if (n < 0 || n > 15)
-        return;
+	if (n < 0 || n > 15)
+		return;
 
-    for (row = 0; row <= n; row++)
-    {
-        for (col = 0; col <= n; col++)
-        {
-            product = row * col;
+	for (row = 0; row <= n; row++)
+	{
+		for (col = 0; col <= n; col++)
+		{
+			product = row * col;
 
-            if (col > 0)
-            {
-                _putchar(',');
-                _putchar(' ');
+			if (col > 0)
+			{
+				_putchar(',');
+				_putchar(' ');
 
-                if (product < 10)
-                    _putchar(' ');
-                if (product < 100)
-                    _putchar(' ');
-            }
+				if (product < 10)
+					_putchar(' ');
+				if (product < 100)
+					_putchar(' ');
+			}
 
-            if (product >= 100)
-            {
-                _putchar((product / 100) + '0');
-                _putchar(((product / 10) % 10) + '0');
-            }
-            else if (product >= 10)
-                _putchar((product / 10) + '0');
-
-            _putchar((product % 10) + '0');
-        }
-        _putchar('\n');
-    }
+			if (product >= 100)
+			{
+				_putchar((product / 100) + '0');
+				_putchar(((product / 10) % 10) + '0');
+			}
+			else if (product >= 10)
+			{
+				_putchar((product / 10) + '0');
+			}
+			_putchar((product % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
+
 
