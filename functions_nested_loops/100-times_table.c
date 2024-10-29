e6a112e804cd981fbb1fbd70e0f4e922933dac63
angelreyes117 <angelreyes.117@gmail.com>
2024-10-29 04:39:00 +0000

    100 times table

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,36 +1,44 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_times_table - Prints the n times table, starting with 0
- * @n: number defining the size of the times table
- *
- * Description: If n is greater than 15 or less than 0,
- * the function does not print anything. Each row in the table
- * has numbers separated by a comma and space, and aligned.
- * Return: Nothing.
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The number defining the times table range.
  */
 void print_times_table(int n)
 {
-	int i, j, product;
+    int row, col, product;
 
-	if (n < 0 || n > 15)
-		return;
+    if (n < 0 || n > 15)
+        return;
 
-	for (i = 0; i <= n; i++)
-	{
-		for (j = 0; j <= n; j++)
-		{
-			product = i * j;
-			if (j != 0)
-				printf(", ");
-			if (product < 10 && j != 0)
-				printf("  %d", product);
-			else if (product < 100 && j != 0)
-				printf(" %d", product);
-			else
-				printf("%d", product);
-		}
-		printf("\n");
-	}
+    for (row = 0; row <= n; row++)
+    {
+        for (col = 0; col <= n; col++)
+        {
+            product = row * col;
+
+            if (col > 0)
+            {
+                _putchar(',');
+                _putchar(' ');
+
+                if (product < 10)
+                    _putchar(' ');
+                if (product < 100)
+                    _putchar(' ');
+            }
+
+            if (product >= 100)
+            {
+                _putchar((product / 100) + '0');
+                _putchar(((product / 10) % 10) + '0');
+            }
+            else if (product >= 10)
+                _putchar((product / 10) + '0');
+
+            _putchar((product % 10) + '0');
+        }
+        _putchar('\n');
+    }
 }
+
