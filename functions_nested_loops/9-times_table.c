c73396008c1a2de3b627cf57ca33e7ad11995219
angelreyes117 <angelreyes.117@gmail.com>
2024-10-29 03:31:09 +0000

    9 times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,33 +7,32 @@
  */
 void times_table(void)
 {
-	int row;
-	int col;
-	int product;
+    int row, col, product;
 
-	for (row = 0; row < 10; row++)
-	{
-		for (col = 0; col < 10; col++)
-		{
-			product = row * col;
+    for (row = 0; row < 10; row++)
+    {
+        for (col = 0; col < 10; col++)
+        {
+            product = row * col;
 
-			if (col == 0)
-			{
-				_putchar(product + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
+            if (col == 0)
+            {
+                _putchar(product + '0');
+            }
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
 
-				if (product < 10)
-				{
-					_putchar(' ');
-				}
+                if (product < 10)
+                {
+                    _putchar(' ');
+                }
 
-				_putchar(product + '0');
-			}
-		}
-		_putchar('\n');
-	}
+                _putchar(product / 10 + '0');
+                _putchar(product % 10 + '0');
+            }
+        }
+        _putchar('\n');
+    }
 }
