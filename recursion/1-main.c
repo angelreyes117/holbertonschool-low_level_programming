235babcb0b10ce6ea28e1e20cbecf97b9abefe12
angelreyes117 <angelreyes.117@gmail.com>
2024-11-16 02:59:10 +0000

    Print rev recursion

---
+++ recursion/1-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/*
+ * main - Entry point for testing the _print_rev_recursion function.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_print_rev_recursion("\nColton Walker");
+	return (0);
+}
