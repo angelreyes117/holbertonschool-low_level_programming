8122b820db000dbd721ca3ae1159a795112cdaab
angelreyes117 <angelreyes.117@gmail.com>
2024-11-16 03:04:46 +0000

    factorial

---
+++ recursion/3-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * main - Entry point for testing the factorial function.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = factorial(1);
+	printf("%d\n", r);
+	r = factorial(5);
+	printf("%d\n", r);
+	r = factorial(10);
+	printf("%d\n", r);
+	r = factorial(-1024);
+	printf("%d\n", r);
+	return (0);
+}
