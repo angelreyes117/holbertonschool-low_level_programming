33fc3ad55f0a8b48fb25c4a1e941e055e027099c
angelreyes117 <angelreyes.117@gmail.com>
2024-11-16 02:55:18 +0000

    Puts Recursion

---
+++ recursion/0-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/* 
+ * main - Entry point for testing the _puts_recursion function.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
