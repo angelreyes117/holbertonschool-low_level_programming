1bac0177e36f2d6ce9b482d8646487e11314e51c
angelreyes117 <angelreyes.117@gmail.com>
2024-11-29 04:57:28 +0000

    Header file

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -4,11 +4,17 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-typedef struct printer{
-char *symbol;
-void (*print)(va_list arg);
+/**
+ * struct printer - A structure that defines a symbol and a function pointer.
+ * @symbol: The symbol representing the type to print (e.g., 'c' for char,
+ *          'i' for int, etc.).
+ * @print: A function pointer to corresponding print function for that type.
+ */
+typedef struct printer
+{
+char *symbol;          /* Symbol representing the type to print */
+void (*print)(va_list arg);  /* Function pointer to the print function */
 } printer_t;
-
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
@@ -19,4 +25,4 @@ void print_float(va_list arg);
 void print_string(va_list arg);
 void print_all(const char * const format, ...);
 
-#endif
+#endif /* VARIADIC_FUNCTIONS_H */
