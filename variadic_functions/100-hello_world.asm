35bb3fa2a342c09ca760f9d79fdcd3efb1b21f4c
angelreyes117 <angelreyes.117@gmail.com>
2024-12-20 05:59:27 +0000

    Hello World

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,21 +1,18 @@
-; 100-hello_world.asm
-; This 64-bit assembly program prints "Hello, World" followed by a newline using the write syscall.
+	global main
 
-section .data
-    msg db "Hello, World", 0xA ; Message with a newline at the end
-    len equ $ - msg            ; Calculate the length of the message
+	section .text
 
-section .text
-    global _start
+main:
+	mov rax, 1        	; write(
+	mov rdi, 1        	;   STDOUT_FILENO,
+	mov rsi, msg      	;   "Hello, Holberton\n",
+	mov rdx, msglen   	;   sizeof("Hello, Holberton\n")
+	syscall           	; );
 
-_start:
-    mov rax, 1                ; syscall: write (sys_write number)
-    mov rdi, 1                ; file descriptor: stdout
-    mov rsi, msg              ; pointer to the message
-    mov rdx, len              ; length of the message
-    syscall                   ; invoke the system call
+	mov rax, 60       	; exit(
+	mov rdi, 0        	;   EXIT_SUCCESS
+	syscall           	; );
 
-    ; Exit the program
-    mov rax, 60               ; syscall: exit (sys_exit number)
-    xor rdi, rdi              ; exit status: 0
-    syscall                   ; invoke the system call
+	section .rodata
+msg:	 db "Hello, Holberton", 10
+msglen:	 equ $ - msg
