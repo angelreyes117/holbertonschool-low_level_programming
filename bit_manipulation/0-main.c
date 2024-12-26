876004a4f91264e26503752100130dd95dd206f0
angelreyes117 <angelreyes.117@gmail.com>
2024-12-26 22:37:24 +0000

    binary to int

---
+++ bit_manipulation/0-main.c
@@ -0,0 +1,28 @@
+/* 0-main.c
+ * Test cases for the binary_to_uint function
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point to test the binary_to_uint function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned int n;
+
+	n = binary_to_uint("1");
+	printf("%u\n", n);
+	n = binary_to_uint("101");
+	printf("%u\n", n);
+	n = binary_to_uint("1e01");
+	printf("%u\n", n);
+	n = binary_to_uint("1100010");
+	printf("%u\n", n);
+	n = binary_to_uint("0000000000000000000110010010");
+	printf("%u\n", n);
+	return (0);
+}
